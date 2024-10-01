5959e9cbb366f1c71c4133a2d2164c2c7e13438e
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2024-10-01 02:01:03 +0200

    Update index.ts

--- apps/www/lib/storage/index.ts
+++ apps/www/lib/storage/index.ts
@@ -1,11 +1,5 @@
 import { sql } from '@vercel/postgres';
 import { drizzle } from 'drizzle-orm/vercel-postgres';
-import { config } from 'dotenv';
 import * as schema from './schema';
-
-const isProduction = process.env.VERCEL_ENV === 'production';
-if (!isProduction) {
-    config({ path: '.env.development.local' });
-}
 
 export const storage = drizzle(sql, { schema });
