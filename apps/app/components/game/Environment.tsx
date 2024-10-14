b01e4d7eb1d4626fd06ab0bf91c2c347bcec3e77
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2024-10-14 09:27:34 +0200

    feat(garden): Applied current time as time of day

--- apps/app/components/game/Environment.tsx
+++ apps/app/components/game/Environment.tsx
@@ -1,68 +1,81 @@
 'use client';
 
 import * as THREE from 'three';
-import { useFrame } from '@react-three/fiber';
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import chroma from 'chroma-js';
 
+const backgroundColorScale = chroma
+    .scale(['#2D3947', '#BADDf6', '#E7E2CC', '#E7E2CC', '#f8b195', '#c06c84', '#6c5b7b', '#2D3947'])
+    .domain([0.2, 0.225, 0.25, 0.75, 0.775, 0.8, 0.825, 0.85]);
+const sunTemperatureScale = chroma
+    .scale([chroma.temperature(20000), chroma.temperature(8000), chroma.temperature(6000), chroma.temperature(6000), chroma.temperature(2000), chroma.temperature(20000)])
+    .domain([0.2, 0.3, 0.75, 0.85]);
+const sunIntensityTimeScale = chroma
+    .scale(['black', 'white', 'white', 'black'])
+    .domain([0.2, 0.25, 0.75, 0.85]);
+const hemisphereSkyColorScale = chroma
+    .scale([chroma.temperature(20000), chroma.temperature(2000), chroma.temperature(20000), chroma.temperature(20000), chroma.temperature(2000), chroma.temperature(20000)])
+    .domain([0, 0.2, 0.3, 0.75, 0.8, 0.85]);
+
+function getTimeOfDay() {
+    const date = new Date();
+    return (date.getHours() * 60 + date.getMinutes()) / (24 * 60);
+}
+
 export function Environment() {
-    const timeOfDay = useRef(0.5);
-
-    const sunDebugRef = useRef<THREE.MeshBasicMaterial>(null);
-    const directionalLightRef = useRef<THREE.DirectionalLight>(null);
-
-    const backgroundDebugRef = useRef<THREE.MeshBasicMaterial>(null);
-    const backgroundRef = useRef<THREE.Color>(null);
-
-    const ambientRef = useRef<THREE.AmbientLight>(null);
-
-    // chroma
-    //     .scale(['yellow', 'lightgreen', '008ae5'])
-    //     .domain([0, 0.25, 1]);
-
-    useFrame((_state, delta) => {
-        // Advance time of day
-        timeOfDay.current = (timeOfDay.current + delta / 10) % 1;
-
-        const sunTemperature = chroma
-            // .temperature(timeOfDay.current * 4000 + 2000);
-            .scale([chroma.temperature(8000), chroma.temperature(6000), chroma.temperature(6000), chroma.temperature(2000)])
-            .domain([0.2, 0.3, 0.7, 0.85])(timeOfDay.current)
-            .hex();
-
-        const sunIntensity = chroma
-            .scale(['black', 'white', 'white', 'black'])
-            .domain([0.2, 0.3, 0.7, 0.85])(timeOfDay.current)
-            .get('rgb.r') / 255;
-        if (directionalLightRef.current)
-            directionalLightRef.current.intensity = sunIntensity * 5;
-        if (ambientRef.current)
-            ambientRef.current.intensity = sunIntensity * 2.5 + 0.5;
-
-        directionalLightRef.current?.color.set(sunTemperature);
-        sunDebugRef.current?.color.set(sunTemperature);
-
-        const backgroundColor = chroma
-            .scale(['#2D3947', '#BADDf6', '#E7E2CC', '#E7E2CC', '#f8b195', '#c06c84', '#6c5b7b', '#2D3947'])
-            .domain([0.2, 0.225, 0.25, 0.75, 0.775, 0.8, 0.825, 0.85])(timeOfDay.current)
-            .hex();
-        backgroundRef.current?.set(backgroundColor);
-        backgroundDebugRef.current?.color.set(backgroundColor);
-    });
-
-    const backgroundColor = new THREE.Color(0xE7E2CC);
     const cameraShadowSize = 100;
     const shadowMapSize = 10;
 
+    const backgroundRef = useRef<THREE.Color>(null);
+    const ambientRef = useRef<THREE.AmbientLight>(null);
+    const hemisphereRef = useRef<THREE.HemisphereLight>(null);
+    const directionalLightRef = useRef<THREE.DirectionalLight>(null);
+
+    useEffect(() => {
+        const timeOfDay = getTimeOfDay();
+
+        const sunIntensity = sunIntensityTimeScale(timeOfDay).get('rgb.r') / 255;
+        if (directionalLightRef.current)
+            directionalLightRef.current.intensity = sunIntensity * 5;
+        if (ambientRef.current)
+            ambientRef.current.intensity = sunIntensity * 2 + 1;
+
+        const sunTemperature = sunTemperatureScale(timeOfDay).rgb();
+        directionalLightRef.current?.color.setRGB(
+            sunTemperature[0] / 255,
+            sunTemperature[1] / 255,
+            sunTemperature[2] / 255,
+            'srgb');
+
+        const backgroundColor = backgroundColorScale(timeOfDay).rgb();
+        backgroundRef.current?.setRGB(
+            backgroundColor[0] / 255,
+            backgroundColor[1] / 255,
+            backgroundColor[2] / 255,
+            'srgb');
+
+        const hemisphereSkyColor = hemisphereSkyColorScale(timeOfDay).rgb();
+        hemisphereRef.current?.color.setRGB(
+            hemisphereSkyColor[0] / 255 * -0,
+            hemisphereSkyColor[1] / 255,
+            hemisphereSkyColor[2] / 255,
+            'srgb');
+        hemisphereRef.current?.groundColor.setRGB(
+            backgroundColor[0] / 255 * 0.5,
+            backgroundColor[1] / 255 * 0.5,
+            backgroundColor[2] / 255 * 0.5,
+            'srgb');
+    }, []);
+
     return (
         <>
-            <color ref={backgroundRef} attach="background" args={[backgroundColor]} />
+            <color ref={backgroundRef} attach="background" />
             <ambientLight ref={ambientRef} intensity={3} />
-            <hemisphereLight
-                color={0xffffbb}
-                groundColor={0x360E0E}
-                intensity={5}
-                position={[0, 1, 0]}
+            <hemisphereLight ref={hemisphereRef} position={[0, 1, 0]} intensity={3} />
+            <directionalLight
+                intensity={1}
+                color={0xecfaff}
+                position={[-10, 10, 10]}
             />
             {/* TODO: Update shadow camera position based on camera position */}
             <directionalLight
@@ -79,15 +92,6 @@ export function Environment() {
                 shadow-camera-near={0.01}
                 shadow-camera-far={1000}
                 castShadow />
-            {/* Debug colors */}
-            <mesh position={[-0, 0, 5]}>
-                <sphereGeometry args={[0.1, 32, 32]} />
-                <meshBasicMaterial ref={sunDebugRef} />
-            </mesh>
-            <mesh position={[-0, 0, 6]}>
-                <sphereGeometry args={[0.1, 32, 32]} />
-                <meshBasicMaterial ref={backgroundDebugRef} />
-            </mesh>
         </>
     );
 }
