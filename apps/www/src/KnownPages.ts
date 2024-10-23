de75e69fa8580e498522acf2901deb54dafc76d1
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2024-10-23 02:16:25 +0200

    feat(www): Added blokovi and blok pages

--- apps/www/src/KnownPages.ts
+++ apps/www/src/KnownPages.ts
@@ -2,6 +2,8 @@ export const KnownPages = {
     Landing: '/',
 
     Plants: '/biljke',
+    Blocks: '/blokovi',
+    Block: (alias: string) => `/blokovi/${alias}`,
     AboutUs: '/o-nama',
     FAQ: '/cesta-pitanja',
 
