25d64b0022844a9497eca7f96a3326ff6ba9491f
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2024-10-24 22:57:00 +0200

    feat: Added playwright tests for apps
    Closes #4

---
+++ apps/farm/playwright.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig, devices, PlaywrightTestConfig } from '@playwright/experimental-ct-react';
+
+export const config: PlaywrightTestConfig = {
+    testDir: './',
+    snapshotDir: './__snapshots__',
+    timeout: 10 * 1000,
+    fullyParallel: true,
+    forbidOnly: !!process.env.CI,
+    retries: process.env.CI ? 2 : 0,
+    workers: process.env.CI ? 1 : undefined,
+    reporter: 'html',
+    use: {
+        baseURL: 'http://127.0.0.1:3000',
+        trace: 'on-first-retry',
+        ctPort: 3100,
+    },
+    projects: [
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome'] },
+        }
+    ],
+    webServer: {
+        command: 'pnpm dev',
+        url: 'http://127.0.0.1:3000',
+        reuseExistingServer: !process.env.CI,
+    },
+};
+
+export default defineConfig(config);
