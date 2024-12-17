20100d3db295706c83cf855d1321d0970be09d4b
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2024-12-18 00:11:23 +0100

    feat(app): Attributes UI refinements

--- apps/app/app/admin/directories/[entityType]/attribute-definitions/AttributeDefinitionsList.tsx
+++ apps/app/app/admin/directories/[entityType]/attribute-definitions/AttributeDefinitionsList.tsx
@@ -1,14 +1,42 @@
 import { ExtendedAttributeDefinition, SelectAttributeDefinitionCategory, getAttributeDefinitions, getAttributeDefinitionCategories } from "@gredice/storage";
-import { Card, CardContent } from "@signalco/ui-primitives/Card";
-import { Chip } from "@signalco/ui-primitives/Chip";
+import { Card } from "@signalco/ui-primitives/Card";
 import { Row } from "@signalco/ui-primitives/Row";
 import { Stack } from "@signalco/ui-primitives/Stack";
-import { Tooltip, TooltipTrigger, TooltipContent } from "@signalco/ui-primitives/Tooltip";
 import { Typography } from "@signalco/ui-primitives/Typography";
-import { Info, Bookmark, BookA } from "lucide-react";
+import { Bookmark, BookA, Binary, Type, ToggleRight, Hash } from "lucide-react";
 import { NoDataPlaceholder } from "../../../../../components/shared/placeholders/NoDataPlaceholder";
 import { KnownPages } from "../../../../../src/KnownPages";
+import { SplitView } from "@signalco/ui/SplitView";
 import Link from "next/link";
+import { HTMLAttributes } from "react";
+
+function AttributeDataTypeIcon({ dataType, ...rest }: { dataType: string } & HTMLAttributes<SVGElement>) {
+    switch (dataType) {
+        case 'text': return <Type {...rest} />;
+        case 'number': return <Hash {...rest} />;
+        case 'boolean': return <ToggleRight {...rest} />;
+        case 'markdown': return (
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                {...rest}
+            >
+                <path d="M2 16V8l4 4 4-4v8" />
+                <path d="M18 8v8" />
+                <path d="m22 12-4 4-4-4" />
+            </svg>
+        );
+        // case 'json': return <Text />;
+        default: return <Binary {...rest} />;
+    }
+}
 
 function AttributeDefinitionCard({ attributeDefinition }: { attributeDefinition: ExtendedAttributeDefinition }) {
     return (
@@ -17,33 +45,14 @@ function AttributeDefinitionCard({ attributeDefinition }: { attributeDefinition:
             passHref
             legacyBehavior>
             <Card>
-                <Row spacing={1} justifyContent="space-between">
+                <Row spacing={1}>
+                    <AttributeDataTypeIcon dataType={attributeDefinition.dataType} className="size-5 text-muted-foreground" />
                     <Stack>
-                        <Typography level="body2">{attributeDefinition.categoryDefinition.label}</Typography>
                         <Typography>
                             {attributeDefinition.label}
                             {attributeDefinition.required && <span className="text-red-600/60 ml-1">*</span>}
                         </Typography>
                         <Typography level="body3" className="line-clamp-2">{attributeDefinition.description}</Typography>
-                    </Stack>
-                    <Stack>
-                        <Chip className="">{
-                            attributeDefinition.dataType.startsWith('json')
-                                ? (
-                                    <Tooltip delayDuration={0}>
-                                        <TooltipTrigger>
-                                            <Row spacing={0.5}>
-                                                <span>json</span>
-                                                <Info className="size-4" />
-                                            </Row>
-                                        </TooltipTrigger>
-                                        <TooltipContent>
-                                            {/* TODO: Show dialog with JSON schema and data picker */}
-                                            <Typography>{attributeDefinition.dataType.substring(5)}</Typography>
-                                        </TooltipContent>
-                                    </Tooltip>
-                                ) : attributeDefinition.dataType}
-                        </Chip>
                     </Stack>
                 </Row>
             </Card>
@@ -75,27 +84,35 @@ export async function AttributeDefinitionsList({ entityTypeName }: { entityTypeN
     const attributeDefinitionCategories = await getAttributeDefinitionCategories(entityTypeName);
 
     return (
-        <Stack spacing={2}>
-            <Row spacing={1}>
-                <Bookmark className="size-5 text-tertiary-foreground" />
-                <Typography level="body2" className="">Kategorije</Typography>
-            </Row>
-            <Stack spacing={1}>
-                {attributeDefinitionCategories.length <= 0 && <NoDataPlaceholder />}
-                {attributeDefinitionCategories.map(c => (
-                    <AttributeDefinitionCategoryCard key={c.id} attributeDefinitionCategory={c} />
-                ))}
+        <SplitView minSize={220}>
+            <Stack spacing={2} className="mr-4">
+                <Row spacing={1}>
+                    <Bookmark className="size-5 text-tertiary-foreground" />
+                    <Typography level="body2" className="">Kategorije</Typography>
+                </Row>
+                <Stack spacing={1}>
+                    {attributeDefinitionCategories.length <= 0 && <NoDataPlaceholder />}
+                    {attributeDefinitionCategories.map(c => (
+                        <AttributeDefinitionCategoryCard key={c.id} attributeDefinitionCategory={c} />
+                    ))}
+                </Stack>
             </Stack>
-            <Row spacing={1}>
-                <BookA className="size-5 text-tertiary-foreground" />
-                <Typography level="body2" className="">Atributi</Typography>
-            </Row>
-            <Stack spacing={1}>
+            <Stack spacing={2} className="ml-4">
                 {attributeDefinitions.length <= 0 && <NoDataPlaceholder />}
-                {attributeDefinitions.map(a => (
-                    <AttributeDefinitionCard key={a.id} attributeDefinition={a} />
+                {attributeDefinitionCategories.map(category => (
+                    <Stack spacing={1} key={category.id}>
+                        <Row spacing={1}>
+                            <BookA className="size-5 text-tertiary-foreground" />
+                            <Typography level="body2" className="">{category.label}</Typography>
+                        </Row>
+                        {attributeDefinitions
+                            .filter(attribute => attribute.categoryDefinition.id === category.id)
+                            .map(attribute => (
+                                <AttributeDefinitionCard key={attribute.id} attributeDefinition={attribute} />
+                            ))}
+                    </Stack>
                 ))}
             </Stack>
-        </Stack>
+        </SplitView>
     );
 }
