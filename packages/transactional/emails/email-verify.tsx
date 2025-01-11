3d27b18f0167eec7f69c7eb1e2d947cb878c7b5f
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2025-01-11 22:42:30 +0100

    feat(transaction): Added email confirm email template

---
+++ packages/transactional/emails/email-verify.tsx
@@ -0,0 +1,72 @@
+/* eslint-disable tailwindcss/enforces-shorthand */
+/* eslint-disable tailwindcss/classnames-order */
+import {
+    Head,
+    Html,
+    Img,
+    Preview,
+    Section, Tailwind,
+} from '@react-email/components';
+import { PrimaryButton } from '../components/PrimaryButton';
+import { Paragraph } from '../components/Paragraph';
+import { Link } from '../components/Link';
+import { Header } from '../components/Header';
+import { Divider } from '../components/Divider';
+import { Disclaimer } from '../components/Disclaimer';
+import { ContentCard } from '../components/ContentCard';
+
+interface VercelInviteUserEmailProps {
+    email: string;
+    verifyPhrase: string;
+    confirmLink: string;
+    appName: string;
+    appDomain: string;
+}
+
+export default function UserLoginRequestEmail({
+    email = 'login@example.com',
+    confirmLink = 'https://vrt.gredice.com/prijava/potvrda-emaila',
+
+    appName = 'Gredice',
+    appDomain = 'gredice.com'
+}: VercelInviteUserEmailProps) {
+    const previewText = `Login request for ${appName}`;
+
+    return (
+        <Html>
+            <Head />
+            <Preview>{previewText}</Preview>
+            <Tailwind>
+                <ContentCard>
+                    <Section className='flex flex-col items-center'>
+                        <Img
+                            src={`https://www.gredice.com/Logotype - gredice@2x.svg`}
+                            width="189"
+                            height="50"
+                            alt="Gredice Logo"
+                        />
+                    </Section>
+                    <Header>Potvrda email adrese</Header>
+                    <Paragraph>Primili smo zahtjev za potvrdu email adrese: <strong>{email}</strong></Paragraph>
+                    <Paragraph>Ako navedeni email odgovara traženom, klikni na gumb ispod za potvrdu.
+                    </Paragraph>
+                    <Section className="my-[32px] text-center">
+                        <PrimaryButton href={confirmLink}>Potvrdi email</PrimaryButton>
+                    </Section>
+                    <Paragraph>
+                        ili kopiraj i zalijepi ovaj URL u svoj preglednik:{' '}
+                        <Link href={confirmLink}>{confirmLink}</Link>
+                    </Paragraph>
+                    <Divider className="my-[26px]" />
+                    <Disclaimer>
+                        Ovaj email je namjenjen za{' '}
+                        <span className="text-black">{email}</span>. Ako ne očekuješ
+                        ovu potvrdu, možeš zanemariti ovaj email. U slučaju da misliš da je tvoj račun ugrožen,
+                        molimo kontaktiraj nas na{' '}
+                        <Link href={`mailto:security@${appDomain}`}>security@{appDomain}</Link>
+                    </Disclaimer>
+                </ContentCard>
+            </Tailwind>
+        </Html>
+    );
+}
