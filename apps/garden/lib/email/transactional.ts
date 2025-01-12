9dadb73b067b080dbcba6b53d3fac6ed4f2a691b
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2025-01-12 20:26:53 +0100

    Update transactional.ts

--- apps/garden/lib/email/transactional.ts
+++ apps/garden/lib/email/transactional.ts
@@ -6,10 +6,17 @@ import { WelcomeEmailTemplateProps } from '@gredice/transactional/emails/welcome
 import EmailVerifyEmailTemplate from '@gredice/transactional/emails/email-verify';
 import { EmailVerifyEmailTemplateProps } from '@gredice/transactional/emails/email-verify';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+let resend: Resend | null = null;
+
+function getResend() {
+    if (!resend) {
+        resend = new Resend(process.env.RESEND_API_KEY);
+    }
+    return resend;
+}
 
 export async function sendEmailVerify(to: string, config: EmailVerifyEmailTemplateProps) {
-    return await resend.emails.send({
+    return await getResend().emails.send({
         from: 'Gredice <suncokret@obavijesti.gredice.com>',
         to: [to],
         subject: 'Gredice - potvrda email adrese',
@@ -18,7 +25,7 @@ export async function sendEmailVerify(to: string, config: EmailVerifyEmailTempla
 }
 
 export async function sendResetPassword(to: string, config: ResetPasswordEmailTemplateProps) {
-    return await resend.emails.send({
+    return await getResend().emails.send({
         from: 'Gredice <suncokret@obavijesti.gredice.com>',
         to: [to],
         subject: 'Dobrodošli u Gredice',
@@ -27,7 +34,7 @@ export async function sendResetPassword(to: string, config: ResetPasswordEmailTe
 }
 
 export async function sendWelcome(to: string, config: WelcomeEmailTemplateProps) {
-    return await resend.emails.send({
+    return await getResend().emails.send({
         from: 'Gredice <suncokret@obavijesti.gredice.com>',
         to: [to],
         subject: 'Dobrodošli u Gredice',
