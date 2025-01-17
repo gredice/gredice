c9a6e01502286296455cb1e3b9ef69a2fac44bf7
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2025-01-17 16:34:26 +0100

    Update SegmentedProgress.tsx

--- packages/game/src/controls/components/SegmentedProgress.tsx
+++ packages/game/src/controls/components/SegmentedProgress.tsx
@@ -1,6 +1,7 @@
 import { cx } from "@signalco/ui-primitives/cx";
 import { Progress } from "./Progress";
 import { HTMLAttributes } from "react";
+import { Check } from "lucide-react";
 
 export type SegmentedProgressProps = {
     segments: { value: number, indeterminate?: boolean }[]
@@ -16,12 +17,18 @@ export function SegmentedProgress({ className, segments, ...rest }: SegmentedPro
                         value={segment.value}
                         indeterminate={segment.indeterminate}
                         className={cx(
-                            'h-full',
+                            'h-2',
                             index !== 0 && index !== segments.length - 1 && 'rounded-none',
                             index === 0 && 'rounded-r-none',
                             index === segments.length - 1 && 'rounded-l-none'
                         )} />
-                    <div className="w-12 -m-2 rounded-full bg-primary" />
+                    <div className={cx(
+                        "size-4 rounded-full -ml-1 -mt-1 bg-white shrink-0 z-10",
+                        segment.value >= 100 && 'bg-green-900',
+                        segment.indeterminate && 'animate-pulse bg-green-300'
+                    )}>
+                        <Check className={cx("size-4 text-white", segment.value < 100 && 'hidden')} />
+                    </div>
                 </>
             ))}
         </div>
