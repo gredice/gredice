34af54a5e0c58b6dfd03a8c3db7d37c09189409e
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2025-01-28 21:43:02 +0100

    Update EmailsList.tsx

--- apps/app/app/admin/communication/inbox/EmailsList.tsx
+++ apps/app/app/admin/communication/inbox/EmailsList.tsx
@@ -1,16 +1,14 @@
-import { Resend } from "resend";
+// let resend: Resend | null = null;
 
-let resend: Resend | null = null;
-
-function getResend() {
-    if (!resend) {
-        resend = new Resend(process.env.RESEND_API_KEY);
-    }
-    return resend;
-}
+// function getResend() {
+//     if (!resend) {
+//         resend = new Resend(process.env.RESEND_API_KEY);
+//     }
+//     return resend;
+// }
 
 export async function EmailsList() {
-    const resendClient = getResend();
+    // const resendClient = getResend();
     // resendClient.emails.
 
     return (
