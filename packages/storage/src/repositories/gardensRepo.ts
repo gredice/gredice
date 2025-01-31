6b53ce648845ba4f2793af794c3b915a994383f0
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2025-01-31 18:50:44 +0100

    Added some event creation

--- packages/storage/src/repositories/gardensRepo.ts
+++ packages/storage/src/repositories/gardensRepo.ts
@@ -2,9 +2,25 @@ import 'server-only';
 import { and, eq } from "drizzle-orm";
 import { storage } from "..";
 import { gardens, InsertGarden, UpdateGarden } from "../schema/gardenSchema";
+import { createEvent, knownEvents } from './eventsRepo';
 
 export async function createGarden(garden: InsertGarden) {
-    await storage.insert(gardens).values(garden);
+    const createdGarden = (await storage
+        .insert(gardens)
+        .values(garden)
+        .returning({ id: gardens.id, name: gardens.name, accountId: gardens.accountId }))[0];
+    if (!createdGarden) {
+        throw new Error('Failed to create garden');
+    }
+
+    await createEvent(knownEvents.gardens.createdV1(
+        createdGarden.id.toString(),
+        {
+            name: createdGarden.name,
+            accountId: createdGarden.accountId
+        }));
+
+    return createdGarden;
 }
 
 export async function getAccountGardens(accountId: string) {
