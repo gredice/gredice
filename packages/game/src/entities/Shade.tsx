821245c77adfd0d8ca45bea85e7bec92d566c340
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2025-02-18 02:45:25 +0100

    feat(garden): Shade variants

--- packages/game/src/entities/Shade.tsx
+++ packages/game/src/entities/Shade.tsx
@@ -9,19 +9,76 @@ import { models } from "../data/models";
 export function Shade({ stack, block, rotation }: EntityInstanceProps) {
     const { nodes, materials }: any = useGameGLTF(models.GameAssets.url);
 
-    let variant = "Solo";
     let realizedRotation = rotation % 2;
+
+    let solo = false;
+    let left = false;
+    let right = false;
+    let n = false;
+    let e = false;
+    let w = false;
+    let s = false;
+    let middle = false;
+
     const neighbors = getEntityNeighbors(stack, block);
-    const nInline = neighbors.n && realizedRotation === 0 && (neighbors.nr % 2) === 0;
-    const eInline = neighbors.e && realizedRotation === 1 && (neighbors.er % 2) === 1;
-    const wInline = neighbors.w && realizedRotation === 1 && (neighbors.wr % 2) === 1;
-    const sInline = neighbors.s && realizedRotation === 0 && (neighbors.sr % 2) === 0;
-    if (neighbors.total >= 2 && ((nInline && sInline) || (eInline && wInline))) {
-        variant = "Middle";
-    } else if (nInline || eInline) {
-        variant = "End_Left";
-    } else if (wInline || sInline) {
-        variant = "End_Right";
+    if (neighbors.total === 1) {
+        if (neighbors.n) {
+            left = true;
+            if (realizedRotation % 2 === 0) {
+                s = true;
+            } else {
+                right = true;
+                w = true;
+                middle = true;
+            }
+        } else if (neighbors.e) {
+            left = true;
+            if (realizedRotation % 2 === 1) {
+                s = true;
+            } else {
+                right = true;
+                e = true;
+                middle = true;
+            }
+        } else if (neighbors.s) {
+            right = true;
+            if (realizedRotation % 2 === 0) {
+                n = true;
+            } else {
+                left = true;
+                e = true;
+                middle = true;
+            }
+        } else if (neighbors.w) {
+            right = true;
+            if (realizedRotation % 2 === 1) {
+                n = true;
+            } else {
+                left = true;
+                w = true;
+                middle = true;
+            }
+        }
+    } else if (neighbors.total >= 2) {
+        middle = true;
+
+        if (neighbors.n) {
+            s = true;
+        }
+        if (neighbors.w) {
+            w = true;
+        }
+        if (neighbors.e) {
+            e = true;
+        }
+        if (neighbors.s) {
+            n = true;
+        }
+        realizedRotation = 0;
+    }
+
+    if (!left && !right && !n && !s && !e && !w && !middle) {
+        solo = true;
     }
 
     const [animatedRotation] = useAnimatedEntityRotation(realizedRotation);
@@ -31,12 +88,54 @@ export function Shade({ stack, block, rotation }: EntityInstanceProps) {
         <animated.group
             position={stack.position.clone().setY(stackHeight(stack, block) + 1)}
             rotation={animatedRotation as unknown as [number, number, number]}>
-            <mesh
+            {solo && <mesh
                 castShadow
                 receiveShadow
-                geometry={nodes[`Shade_${variant}`].geometry}
+                geometry={nodes[`Shade_Solo`].geometry}
                 material={materials['Material.Planks']}
-            />
+            />}
+            {left && <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes[`Shade_Single_Left`].geometry}
+                material={materials['Material.Planks']}
+            />}
+            {right && <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes[`Shade_Single_Right`].geometry}
+                material={materials['Material.Planks']}
+            />}
+            {n && <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes['Shade_N'].geometry}
+                material={materials['Material.Planks']}
+            />}
+            {e && <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes['Shade_E'].geometry}
+                material={materials['Material.Planks']}
+            />}
+            {w && <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes['Shade_W'].geometry}
+                material={materials['Material.Planks']}
+            />}
+            {s && <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes['Shade_S'].geometry}
+                material={materials['Material.Planks']}
+            />}
+            {middle && <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes['Shade_Middle'].geometry}
+                material={materials['Material.Planks']}
+            />}
             {/* @ts-ignore */}
         </animated.group>
     );
