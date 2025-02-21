bdfffba5b1c8445756a26b26c8139d81752e5966
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2025-02-21 17:20:42 +0100

    feat(assets): Added script for exporting GLB from Blender on Windows

---
+++ assets/export.ps1
@@ -0,0 +1,1 @@
+& "C:\Program Files\Blender Foundation\Blender 4.3\blender.exe" '-b' './GameAssets.blend' '--python-expr' 'import bpy; bpy.ops.export_scene.gltf(filepath=''../apps/garden/public/assets/models/GameAssets.glb'',export_apply=True)'
