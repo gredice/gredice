0c09abebcceb4c47958144574ae7bee3583cea9f
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2025-03-15 11:46:11 +0100

    feat: Manifest adjustments

--- apps/garden/app/layout.tsx
+++ apps/garden/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning={true}>
+    <html lang="hr" suppressHydrationWarning={true}>
       <Head>
         <meta name="theme-color" content="#2e6f40" />
         <meta name="apple-mobile-web-app-title" content="Gredice" />
