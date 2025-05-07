06a0a154731ebdef15a18b40e34fe548d94a4d3d
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2025-05-07 11:37:53 +0200

    feat(game): Removed unused exports

--- packages/game/src/index.ts
+++ packages/game/src/index.ts
@@ -3,5 +3,3 @@ export type { GameSceneProps } from './GameScene';
 export { GardenLoadingIndicator } from './GardenLoadingIndicator';
 export { EntityViewer } from './viewers/EntityViewer';
 export type { EntityViewerProps } from './viewers/EntityViewer';
-export { environmentState } from './scene/Environment';
-export { useGameState } from './useGameState';
