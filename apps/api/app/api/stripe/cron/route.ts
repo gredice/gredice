e6882212d89053f5dedce73ae69cecbf5b6edbcb
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2025-06-14 20:10:52 +0200

    Update route.ts

--- apps/api/app/api/stripe/cron/route.ts
+++ apps/api/app/api/stripe/cron/route.ts
@@ -1,9 +1,10 @@
 import { getStripeCheckoutSessions } from "@gredice/stripe/server";
+import { NextRequest } from "next/server";
 import { processCheckoutSession } from "../../../../lib/stripe/processCheckoutSession";
 
 export const dynamic = 'force-dynamic';
 
-export async function GET() {
+export async function GET(request: NextRequest) {
     const authHeader = request.headers.get('authorization');
     if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
         return new Response('Unauthorized', {
@@ -11,7 +12,7 @@ export async function GET() {
         });
     }
 
-    const yesterday = new Date();
+    let yesterday = new Date();
     yesterday.setDate(yesterday.getDate() - 3);
     const checkoutSessions = await getStripeCheckoutSessions(yesterday);
     await Promise.all(checkoutSessions.map(s => s.id).map(processCheckoutSession));
