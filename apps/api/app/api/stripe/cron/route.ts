a272bbcd5baead13612e5b1ec4fc0f4d34292876
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2025-06-14 20:10:03 +0200

    Update route.ts

--- apps/api/app/api/stripe/cron/route.ts
+++ apps/api/app/api/stripe/cron/route.ts
@@ -11,7 +11,7 @@ export async function GET() {
         });
     }
 
-    let yesterday = new Date();
+    const yesterday = new Date();
     yesterday.setDate(yesterday.getDate() - 3);
     const checkoutSessions = await getStripeCheckoutSessions(yesterday);
     await Promise.all(checkoutSessions.map(s => s.id).map(processCheckoutSession));
