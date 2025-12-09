9e775b3cffe50fa00fc87afa73d5e8d164961f49
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2025-12-09 14:57:37 +0100

    Add Nuqs adapter to www provider

--- apps/www/components/providers/ClientAppProvider.tsx
+++ apps/www/components/providers/ClientAppProvider.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { NuqsAdapter } from '@gredice/ui/nuqs';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { PropsWithChildren } from 'react';
 import { WinterModeProvider } from './WinterModeProvider';
@@ -8,8 +9,10 @@ export const queryClient = new QueryClient();
 
 export function ClientAppProvider({ children }: PropsWithChildren) {
     return (
-        <QueryClientProvider client={queryClient}>
-            <WinterModeProvider>{children}</WinterModeProvider>
-        </QueryClientProvider>
+        <NuqsAdapter>
+            <QueryClientProvider client={queryClient}>
+                <WinterModeProvider>{children}</WinterModeProvider>
+            </QueryClientProvider>
+        </NuqsAdapter>
     );
 }
