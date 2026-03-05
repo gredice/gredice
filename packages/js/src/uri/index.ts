de28479ca9246b84fea0af400e12dfc16341a82a
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2026-03-05 10:35:07 +0100

    refactor(www): share route param decoding in js uri utils

--- packages/js/src/uri/index.ts
+++ packages/js/src/uri/index.ts
@@ -6,3 +6,29 @@ export function decodeUriComponentSafe(value: string) {
         return value;
     }
 }
+
+/**
+ * Decodes URL percent-encoding and common HTML entities found in route params.
+ */
+export function decodeRouteParam(value: string) {
+    const decodedParam = decodeUriComponentSafe(value);
+
+    return decodedParam.replaceAll(
+        /&(?:amp|apos|quot);|&#(?:39|x27);/gi,
+        (entity) => {
+            const normalizedEntity = entity.toLowerCase();
+            switch (normalizedEntity) {
+                case '&amp;':
+                    return '&';
+                case '&apos;':
+                case '&#39;':
+                case '&#x27;':
+                    return "'";
+                case '&quot;':
+                    return '"';
+                default:
+                    return entity;
+            }
+        },
+    );
+}
