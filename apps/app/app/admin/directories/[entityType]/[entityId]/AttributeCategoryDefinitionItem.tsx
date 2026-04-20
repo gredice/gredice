11f487af937d64f218edebccd708951bb03fc117
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2026-04-20 11:29:34 +0200

    app: move multi-value attribute add button to header

--- apps/app/app/admin/directories/[entityType]/[entityId]/AttributeCategoryDefinitionItem.tsx
+++ apps/app/app/admin/directories/[entityType]/[entityId]/AttributeCategoryDefinitionItem.tsx
@@ -5,6 +5,7 @@ import type {
     SelectAttributeValue,
 } from '@gredice/storage';
 import { Button } from '@signalco/ui-primitives/Button';
+import { Row } from '@signalco/ui-primitives/Row';
 import { Stack } from '@signalco/ui-primitives/Stack';
 import { Typography } from '@signalco/ui-primitives/Typography';
 import { AttributeInput } from '../../../../../components/shared/attributes/AttributeInput';
@@ -43,19 +44,24 @@ export function AttributeCategoryDefinitionItem({
 
     return (
         <Stack key={attributeDefinition.id} spacing={1}>
-            <Stack>
-                <Typography level="body1" semiBold>
-                    {attributeDefinition.label}
-                    {attributeDefinition.required && (
-                        <span className="text-red-600/60 ml-1">*</span>
-                    )}
-                </Typography>
-                {Boolean(attributeDefinition.description?.length) && (
-                    <Typography level="body2">
-                        {attributeDefinition.description}
+            <Row justifyContent="space-between" alignItems="flex-start">
+                <Stack>
+                    <Typography level="body1" semiBold>
+                        {attributeDefinition.label}
+                        {attributeDefinition.required && (
+                            <span className="text-red-600/60 ml-1">*</span>
+                        )}
                     </Typography>
+                    {Boolean(attributeDefinition.description?.length) && (
+                        <Typography level="body2">
+                            {attributeDefinition.description}
+                        </Typography>
+                    )}
+                </Stack>
+                {attributeDefinition.multiple && (
+                    <Button onClick={handleAdd}>Dodaj</Button>
                 )}
-            </Stack>
+            </Row>
             <Stack spacing={1}>
                 {attributeDefinition.multiple ? (
                     entity.attributes
@@ -84,9 +90,6 @@ export function AttributeCategoryDefinitionItem({
                                 attributeDefinition.id,
                         )}
                     />
-                )}
-                {attributeDefinition.multiple && (
-                    <Button onClick={handleAdd}>Dodaj</Button>
                 )}
             </Stack>
         </Stack>
