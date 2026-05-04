c58b4fb073cb92dfe397e40ef07a3704c2949681
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2026-05-04 13:31:58 +0200

    Update app branding and theme colors

--- apps/api/tests/landing.spec.ts
+++ apps/api/tests/landing.spec.ts
@@ -2,5 +2,5 @@ import { expect, test } from '@playwright/test';
 
 test('has title', async ({ page }) => {
     await page.goto('/');
-    await expect(page).toHaveTitle(/API \| Gredice/);
+    await expect(page).toHaveTitle(/Gredice API/);
 });
