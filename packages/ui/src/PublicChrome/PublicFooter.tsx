da3c228a7a2bee49be007d1871a4c4f7c2605be5
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2026-08-18 13:48:38 +0200

    chore(www): rename footer section to Stranice (#4608)

--- packages/ui/src/PublicChrome/PublicFooter.tsx
+++ packages/ui/src/PublicChrome/PublicFooter.tsx
@@ -118,7 +118,7 @@ function sectionsData(linkMode: PublicChromeLinkMode): SectionData[] {
                     ],
                 },
                 {
-                    header: 'Aplikacija',
+                    header: 'Stranice',
                     ctas: [
                         {
                             label: 'Podignuta gredica',
