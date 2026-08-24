d86ebb873d5a49b32ea45a573ca05c434b2779ed
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2026-08-24 23:39:54 +0200

    fix(gardens): preserve existing visibility choices

--- packages/storage/src/migrations/0084_panoramic_lily_hollister.sql
+++ packages/storage/src/migrations/0084_panoramic_lily_hollister.sql
@@ -1,2 +1,1 @@
-ALTER TABLE "gardens" ALTER COLUMN "is_public" SET DEFAULT true;--> statement-breakpoint
-UPDATE "gardens" SET "is_public" = true WHERE "is_public" = false;
+ALTER TABLE "gardens" ALTER COLUMN "is_public" SET DEFAULT true;
