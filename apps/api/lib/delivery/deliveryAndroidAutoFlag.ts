c9d2cd01ab0969468e72e240b4319963ec3600b8
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2026-08-29 23:37:25 +0200

    style(api): remove redundant flag assertion

--- apps/api/lib/delivery/deliveryAndroidAutoFlag.ts
+++ apps/api/lib/delivery/deliveryAndroidAutoFlag.ts
@@ -1,5 +1,4 @@
-export const deliveryAndroidAutoFlagName =
-    'DELIVERY_ANDROID_AUTO_ENABLED' as const;
+export const deliveryAndroidAutoFlagName = 'DELIVERY_ANDROID_AUTO_ENABLED';
 
 export function deliveryAndroidAutoEnabled(
     value = process.env[deliveryAndroidAutoFlagName],
